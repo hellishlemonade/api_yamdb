eda9c62df066975f8d85ad64c8be48418c055386
Dmitriy Lyapin <hellishlemonade111@gmail.com>
2025-04-02 15:51:15 +0300

    Created ViewSets and endpoints for registration/Modificated Serializer

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import GenreViewSet, TitleViewSet
+from .views import GenreViewSet, TitleViewSet, SignUpViewSet, TokenViewSet
 
 VERSION = 'v1'
 
 router = SimpleRouter()
 router.register('titles', TitleViewSet, basename='titles')
 router.register('genres', GenreViewSet, basename='genres')
+router.register('auth', SignUpViewSet)
+router.register('token', TokenViewSet)
 
 urlpatterns = [
     path(f'{VERSION}/', include(router.urls)),
