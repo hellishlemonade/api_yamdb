ef51f3d19e090289c0e7cf3b80ce005ca555e034
Aleksandr Poddyachiy <4bestu@gmail.com>
2025-04-06 16:59:27 +0300

    Review-comment fix after 1st review (1.1)

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -39,12 +39,13 @@ class ContentManagePermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if request.user.is_authenticated:
-            return (
-                obj.author == request.user
-                or request.user.is_moderator()
-                or request.user.is_admin()
-                )
+        if not request.user.is_authenticated:
+            return False
+        return (
+            obj.author == request.user
+            or request.user.is_moderator()
+            or request.user.is_admin()
+            )
 
 
 class IsAdminPermission(permissions.BasePermission):
